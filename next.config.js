fc8ef69593434ca0321e29fd068ff71a554b585f
Nozililo Emily Nkuna <nozililoemilynkuna@gmail.com>
2025-09-02 20:16:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -18,6 +18,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // This is to fix a build issue with the get-audio-duration library.
+    // It attempts to import a README.md file, which webpack can't handle.
+    config.module.rules.push({
+      test: /README\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
